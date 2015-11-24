3cab0ba593c90b5fbeafa0ae85a6af604a1835c2
Sage Elliott <wildtroll@gmail.com>
2015-11-23 20:59:19 -0800

    Feat: Tool - Use gulpjs to:
    - minify and concat css files
    - minify js files
    - optimize images

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,56 @@
 var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var minifyCSS = require('gulp-minify-css');
+var imagemin = require('gulp-imagemin');
 
+// Concat multiple JS files
+gulp.task("concatScripts", function(){
+    gulp.src([
+        'js/file1.js',
+        'js/file2.js',
+        'js/file2.js'
+        ])
+    .pipe(concat(app.js))
+    .pipe(gulp.dest('js'));
+});
+
+// Minify JS files used in index.
+gulp.task('minifyScripts', function() {
+    gulp.src("js/perfmatters.js")
+        .pipe(uglify())
+        .pipe(rename('app.min.js'))
+        .pipe(gulp.dest('js'));
+});
+
+// Concat CSS
+gulp.task('concatCSS', function() {
+    gulp.src([
+        'css/print.css',
+        'css/style.css'
+        ])
+        .pipe(rename('main.css'))
+        .pipe(gulp.dest('css'));
+});
+
+// Minify CSS
+gulp.task('minifyCSS', function() {
+    gulp.src("css/main.css")
+        .pipe(minifyCSS())
+        .pipe(rename('main.min.css'))
+        .pipe(gulp.dest('css'));
+});
+
+// optimize images
+gulp.task('images', function(){
+  return gulp.src(['img/**/*.+(png|jpg|gif|svg)',
+                    'views/images//**/*.+(png|jpg|gif|svg)'])
+  .pipe(imagemin())
+  .pipe(gulp.dest('dist/images'))
+});
+
+// test task
 gulp.task('hello', function() {
   console.log('Hello Zell');
 });
